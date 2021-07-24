98216f5b71baff6ff2ee6f21beda36b7ba50f95f
renato-jgs <renato_jgs@hotmail.com>
2021-07-24 05:56:32 -0300

    Criação das DTOs das entidades.

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
@@ -16,7 +17,7 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
         Person savedPerson = personRepository.save(person);
         return MessageResponseDTO.builder().message("Create person with ID: " + savedPerson.getId()).build();
     }
